19a87707e4c794f29c71bbb9b9004387022da654
grace550213 <59430130+grace550213@users.noreply.github.com>
2020-03-09 23:27:31 +0800

    update

--- js/all.js
+++ js/all.js
@@ -4,9 +4,14 @@ jQuery(document).ready(function($) {
     $(this).find('.card-body').find('h5').addClass('hover')
   });
 
-  $('.card').mouseleave(function(event) {
+    $('.card').mouseleave(function(event) {
     /* Act on the event */
      $(this).find('.card-body').find('h5').removeClass('hover')
   });
+
+    $('.gotop').click(function(event) {
+    	/* Act on the event */
+    	$('html, body').scrollTop(0);
+    });
 });
 
